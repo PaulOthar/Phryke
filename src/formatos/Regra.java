ec78dc552e444cddaebd7038208e6439aafa4dec
PaulOthar <xpaul.e.j@gmail.com>
2020-07-15 19:55:23 -0300

    Adicionei Codificador e decodificador a regra

--- src/formatos/Regra.java
+++ src/formatos/Regra.java
@@ -3,6 +3,12 @@ package formatos;
 public class Regra extends Formato {
 	private String regra,sistema;
 
+	public Regra() {
+		super.setNome(new String());
+		this.setRegra(new String());
+		this.setSistema(new String());
+	}
+	
 	public String getRegra() {
 		return regra;
 	}
@@ -16,15 +22,30 @@ public class Regra extends Formato {
 	public void setSistema(String sistema) {
 		this.sistema = sistema;
 	}
+	
 	@Override
 	public String DeDadosParaCodigo() {
-		// TODO Auto-generated method stub
-		return null;
+		String Codigo = new String();
+		Codigo = super.getNome()+primario+this.getRegra()+primario+this.getSistema();
+		return Codigo;
 	}
 	@Override
 	public void DeCodigoParaDados(String Codigo) {
-		// TODO Auto-generated method stub
-		
+		int Contador = 0;
+		for(String s : Codigo.split(primario)) {
+			switch(Contador) {
+			case 0:
+				super.setNome(s);
+				break;
+			case 1:
+				this.setRegra(s);
+				break;
+			case 2:
+				this.setSistema(s);
+				break;
+			}
+			Contador++;
+		}
 	}
 	
 }
--- src/main/Teste.java
+++ src/main/Teste.java
@@ -1,14 +1,7 @@
 package main;
-
-import formatos.Caracteristica;
-import formatos.Peculiaridade;
-import formatos.Raca;
-import formatos.Sistema;
-
 public class Teste {
 
 	public static void main(String[] args) {
-		System.out.println(new Sistema().DeDadosParaCodigo());
 	}
 
 }
